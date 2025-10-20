39274943db8e6f8797faa5c0350c192dbd098d16
emrecanardc <ardcemre133@gmail.com>
2025-10-20 13:10:53 +0300

    Hoş Geldiniz

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("data"),
-    );
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            child: Text("UniHub'a Hoş Geldiniz!"),
+          )
+          ),
+        )
+        
+      );
+    
   }
 }
